6ef65427858f954632be2689c42556ed740ce4db
Ding Wei <barbarum@users.noreply.github.com>
2023-03-24 13:47:06 +0800

    Update model to text-ada-001
    Signed-off-by: Ding Wei <barbarum@users.noreply.github.com>

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -27,7 +27,7 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "text-ada-001",
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
@@ -49,8 +49,7 @@ export default async function (req, res) {
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
